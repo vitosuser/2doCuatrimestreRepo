b70e9b2925bab083e8ee581ac28c0cf060d9bc1c
vito <vitoromanloro@gmail.com>
2025-09-07 00:37:43 -0300

    continue con el ej de notas

--- 2doCuatrimestre/EjNotaEscolares/Form1.Designer.cs
+++ 2doCuatrimestre/EjNotaEscolares/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EjNotaEscolares
 {
-    partial class Form1
+    partial class formNotas
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            lblAlumno = new Label();
+            lblMateria = new Label();
+            txtName = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            txtMateria = new TextBox();
+            txtNota = new TextBox();
+            lblNota = new Label();
+            gboxTipo = new GroupBox();
+            rdioTP = new RadioButton();
+            rdioParcial = new RadioButton();
+            rdioFinal = new RadioButton();
+            mnstrp1 = new MenuStrip();
+            notasToolStripMenuItem = new ToolStripMenuItem();
+            planillaToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            btn = new Button();
+            ctxMenu = new ContextMenuStrip(components);
+            limpiarEntradasToolStripMenuItem = new ToolStripMenuItem();
+            gboxTipo.SuspendLayout();
+            mnstrp1.SuspendLayout();
+            ctxMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblAlumno
+            // 
+            lblAlumno.AutoSize = true;
+            lblAlumno.Font = new Font("Segoe UI", 12F);
+            lblAlumno.Location = new Point(44, 82);
+            lblAlumno.Name = "lblAlumno";
+            lblAlumno.Size = new Size(153, 21);
+            lblAlumno.TabIndex = 0;
+            lblAlumno.Text = "Nombre del alumno:";
+            // 
+            // lblMateria
+            // 
+            lblMateria.AutoSize = true;
+            lblMateria.Font = new Font("Segoe UI", 12F);
+            lblMateria.Location = new Point(44, 149);
+            lblMateria.Name = "lblMateria";
+            lblMateria.Size = new Size(70, 21);
+            lblMateria.TabIndex = 1;
+            lblMateria.Text = "Materia: ";
+            lblMateria.Click += lblMateria_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(203, 82);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(197, 23);
+            txtName.TabIndex = 2;
+            // 
+            // txtMateria
+            // 
+            txtMateria.Location = new Point(120, 151);
+            txtMateria.Name = "txtMateria";
+            txtMateria.Size = new Size(197, 23);
+            txtMateria.TabIndex = 3;
+            // 
+            // txtNota
+            // 
+            txtNota.Location = new Point(112, 217);
+            txtNota.Name = "txtNota";
+            txtNota.Size = new Size(106, 23);
+            txtNota.TabIndex = 4;
+            txtNota.TextAlign = HorizontalAlignment.Right;
+            // 
+            // lblNota
+            // 
+            lblNota.AutoSize = true;
+            lblNota.Font = new Font("Segoe UI", 12F);
+            lblNota.Location = new Point(44, 215);
+            lblNota.Name = "lblNota";
+            lblNota.Size = new Size(47, 21);
+            lblNota.TabIndex = 5;
+            lblNota.Text = "Nota:";
+            lblNota.Click += label1_Click;
+            // 
+            // gboxTipo
+            // 
+            gboxTipo.Controls.Add(rdioFinal);
+            gboxTipo.Controls.Add(rdioParcial);
+            gboxTipo.Controls.Add(rdioTP);
+            gboxTipo.Location = new Point(44, 272);
+            gboxTipo.Name = "gboxTipo";
+            gboxTipo.Size = new Size(200, 129);
+            gboxTipo.TabIndex = 6;
+            gboxTipo.TabStop = false;
+            gboxTipo.Text = "Tipo";
+            // 
+            // rdioTP
+            // 
+            rdioTP.AutoSize = true;
+            rdioTP.Location = new Point(15, 34);
+            rdioTP.Name = "rdioTP";
+            rdioTP.Size = new Size(109, 19);
+            rdioTP.TabIndex = 0;
+            rdioTP.TabStop = true;
+            rdioTP.Text = "Trabajo Practico";
+            rdioTP.UseVisualStyleBackColor = true;
+            // 
+            // rdioParcial
+            // 
+            rdioParcial.AutoSize = true;
+            rdioParcial.Location = new Point(15, 59);
+            rdioParcial.Name = "rdioParcial";
+            rdioParcial.Size = new Size(60, 19);
+            rdioParcial.TabIndex = 1;
+            rdioParcial.TabStop = true;
+            rdioParcial.Text = "Parcial";
+            rdioParcial.UseVisualStyleBackColor = true;
+            // 
+            // rdioFinal
+            // 
+            rdioFinal.AutoSize = true;
+            rdioFinal.Location = new Point(15, 84);
+            rdioFinal.Name = "rdioFinal";
+            rdioFinal.Size = new Size(50, 19);
+            rdioFinal.TabIndex = 2;
+            rdioFinal.TabStop = true;
+            rdioFinal.Text = "Final";
+            rdioFinal.UseVisualStyleBackColor = true;
+            // 
+            // mnstrp1
+            // 
+            mnstrp1.Items.AddRange(new ToolStripItem[] { notasToolStripMenuItem, ayudaToolStripMenuItem, salirToolStripMenuItem });
+            mnstrp1.Location = new Point(0, 0);
+            mnstrp1.Name = "mnstrp1";
+            mnstrp1.Size = new Size(447, 24);
+            mnstrp1.TabIndex = 7;
+            mnstrp1.Text = "menuStrip1";
+            // 
+            // notasToolStripMenuItem
+            // 
+            notasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { planillaToolStripMenuItem });
+            notasToolStripMenuItem.Name = "notasToolStripMenuItem";
+            notasToolStripMenuItem.Size = new Size(50, 20);
+            notasToolStripMenuItem.Text = "Notas";
+            // 
+            // planillaToolStripMenuItem
+            // 
+            planillaToolStripMenuItem.Name = "planillaToolStripMenuItem";
+            planillaToolStripMenuItem.Size = new Size(180, 22);
+            planillaToolStripMenuItem.Text = "Planilla";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { acercaDeToolStripMenuItem });
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(53, 20);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(180, 22);
+            acercaDeToolStripMenuItem.Text = "Acerca de";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.RightToLeft = RightToLeft.No;
+            salirToolStripMenuItem.Size = new Size(41, 20);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // btn
+            // 
+            btn.Location = new Point(325, 352);
+            btn.Name = "btn";
+            btn.Size = new Size(75, 23);
+            btn.TabIndex = 8;
+            btn.Text = "Guardar";
+            btn.UseVisualStyleBackColor = true;
+            // 
+            // ctxMenu
+            // 
+            ctxMenu.Items.AddRange(new ToolStripItem[] { limpiarEntradasToolStripMenuItem });
+            ctxMenu.Name = "ctxMenu";
+            ctxMenu.Size = new Size(181, 48);
+            // 
+            // limpiarEntradasToolStripMenuItem
+            // 
+            limpiarEntradasToolStripMenuItem.Name = "limpiarEntradasToolStripMenuItem";
+            limpiarEntradasToolStripMenuItem.Size = new Size(180, 22);
+            limpiarEntradasToolStripMenuItem.Text = "Limpiar entradas";
+            // 
+            // formNotas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GrayText;
+            ClientSize = new Size(447, 450);
+            Controls.Add(btn);
+            Controls.Add(gboxTipo);
+            Controls.Add(lblNota);
+            Controls.Add(txtNota);
+            Controls.Add(txtMateria);
+            Controls.Add(txtName);
+            Controls.Add(lblMateria);
+            Controls.Add(lblAlumno);
+            Controls.Add(mnstrp1);
+            MainMenuStrip = mnstrp1;
+            Name = "formNotas";
+            Text = "Gestor Notas";
+            gboxTipo.ResumeLayout(false);
+            gboxTipo.PerformLayout();
+            mnstrp1.ResumeLayout(false);
+            mnstrp1.PerformLayout();
+            ctxMenu.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAlumno;
+        private Label lblMateria;
+        private TextBox txtName;
+        private System.Windows.Forms.Timer timer1;
+        private TextBox txtMateria;
+        private TextBox txtNota;
+        private Label lblNota;
+        private GroupBox gboxTipo;
+        private RadioButton rdioFinal;
+        private RadioButton rdioParcial;
+        private RadioButton rdioTP;
+        private MenuStrip mnstrp1;
+        private ToolStripMenuItem notasToolStripMenuItem;
+        private ToolStripMenuItem planillaToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private Button btn;
+        private ContextMenuStrip ctxMenu;
+        private ToolStripMenuItem limpiarEntradasToolStripMenuItem;
     }
 }
--- 2doCuatrimestre/EjNotaEscolares/Form1.cs
+++ 2doCuatrimestre/EjNotaEscolares/Form1.cs
@@ -1,10 +1,25 @@
 namespace EjNotaEscolares
 {
-    public partial class Form1 : Form
+    public partial class formNotas : Form
     {
-        public Form1()
+        public formNotas()
         {
             InitializeComponent();
+        }
+
+        private void lblMateria_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
