e565c8d27e5f16652d78f0b839f2b87cab08d44f
vitosuser <vitoromanloro@gmail.com>
2025-10-01 18:55:12 -0300

    iniciio tp est dinamicas 2

---
+++ 2doCuatrimestre/TPEstructurasDinamicas1(tp 2)/EjerciciosList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPEstructurasDinamicas1_tp_2_
+{
+    internal class EjerciciosList
+    {
+    }
+}
---
+++ 2doCuatrimestre/TPEstructurasDinamicas1(tp 2)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TPEstructurasDinamicas1_tp_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
